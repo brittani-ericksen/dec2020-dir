84fffee48f84d95ab6923a7a1c9b95fd4e686e6f
Brittani Ericksen <heybrittani@gmail.com>
2021-01-12 17:03:36 -0500

    week 5 day 2

--- week5/day1/chuck/js/index.js
+++ week5/day1/chuck/js/index.js
@@ -9,6 +9,7 @@ fetch("https://api.chucknorris.io/jokes/random?category=dev")
         // return data;
     })
     .catch(function (error) {
+        console.error("ERROR: ", error);
         return error;
     });
 
---
+++ week5/day2/chuck_modal/js/index.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function getQuote(category) {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    get(url).then(function (response) {
+        updateBody(response.value);
+    })
+};
+
+function getCategories() {
+    const url = "https://api.chucknorris.io/jokes/categories";
+    get(url).then(function (response) {
+        buildCategoryList(response);
+    })
+}
+
+function updateBody(quote) {
+    const paragraph = document.querySelector("#modal p");
+    paragraph.innerHTML = quote;
+    toggleModal();
+}
+
+function buildCategoryList(categoryList) {
+    // filter out the "explicit" category
+    const filteredList = categoryList.filter(function (category) {
+        if (category !== "explicit") {
+            return category;
+        }
+    });
+
+    const form = document.querySelector("#changeQuote");
+    const categorySelect = document.createElement("select");
+    filteredList.map(function(category) {
+        const categoryOption = document.createElement("option");
+        categoryOption.value = category;
+        categoryOption.text = category;
+        categorySelect.appendChild(categoryOption);
+    });
+    form.appendChild(categorySelect);
+
+    categorySelect.addEventListener("change", function (event) {
+        getQuote(event.target.value);
+    })
+}
+
+closeModal.addEventListener('click', toggleModal);
+
+function toggleModal() {
+    const modalOverlay = document.querySelector('#overlay');
+    modalOverlay.classList.toggle('visible');
+}
+
+getCategories();
+getQuote("dev");
